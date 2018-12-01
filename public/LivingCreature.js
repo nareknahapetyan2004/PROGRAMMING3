9382d9d902376321e58c937ef555da20d6afc358
Your Name <you@example.com>
2018-12-01 16:52:48 +0400

    PROGRAMMING3

---
+++ public/LivingCreature.js
@@ -0,0 +1,38 @@
+class LivingCreature {
+    constructor(x, y, index){
+        this.x = x;
+        this.y = y;
+        this.index = index;
+        this.multiply = 0;
+    }
+
+    newDirections() {
+        this.directions = [
+            [this.x + 1, this.y],
+            [this.x + 2, this.y],
+            [this.x - 1, this.y],
+            [this.x - 2, this.y],
+            [this.x, this.y - 1],
+            [this.x, this.y - 2],
+            [this.x, this.y + 1],
+            [this.x, this.y + 2],
+        ];
+    }
+
+    getDirections(t) {
+        this.newDirections();
+        var found = [];
+        
+        for (var i in this.directions) {
+            var x = this.directions[i][0];
+            var y = this.directions[i][1];
+            if (x >= 0 && x < matrix[0].length && y >= 0 && y < matrix.length){
+                if (matrix[y][x] == t) {
+                    found.push(this.directions[i]);
+                }
+            }   
+        }
+        return found;
+    }
+}
+    
--- public/scripts/mard.js
+++ public/scripts/mard.js
@@ -1,119 +1,66 @@
+class Mard extends LivingCreature{
+    constructor(x, y, ind) {
+        super(x, y, ind);
+        this.eatCount = 0;
+        this.energy1 = 10;
+    }
+
+    move() {
+        console.log(this.energy1);
+        var xotCord = this.getDirections(1);
+        var cord1 = random(xotCord);
+
+        if (cord1) {
+            var x = cord1[0];
+            var y = cord1[1];
+
+            matrix[y][x] = 4;
+            matrix[this.y][this.x] = 1;
+
+            this.x = x;
+            this.y = y;
+        }
+        else {
+            var emptyCord = this.getDirections(0);
+            var cord = random(emptyCord);
+
+            if (cord) {
+                var x = cord[0];
+                var y = cord[1];
+
+                matrix[y][x] = 4;
+                matrix[this.y][this.x] = 0;
+
+                this.x = x;
+                this.y = y;
+            }
+        }
+    }
 
 
 
+    eat() {
+        var xotakerCord = this.getDirections(2);
+        var cord1 = random(xotakerCord);
 
+        if (cord1) {
 
-class Mard
-{
-	constructor(x,y,ind)
-	{
-		this.index = ind;
-		this.x = x;
-		this.y = y;
-		this.multiply = 0;
-		this.eatCount = 0;
-		this.energy1 = 10; 
+            var x = cord1[0];
+            var y = cord1[1];
 
-	}
+            matrix[this.y][this.x] = 0;
+            matrix[y][x] = 4;
 
-	newDirections()
-	{
-		this.directions = [
-		    				[this.x + 1, this.y		],
-							[this.x + 2, this.y     ],
-							[this.x - 1, this.y		],
-							[this.x - 2, this.y		],
-							[this.x, 	 this.y - 1 ],
-							[this.x, 	 this.y - 2 ],
-							[this.x, 	 this.y + 1 ],
-							[this.x, 	 this.y + 2 ],
-						];
-	}
+            this.x = x;
+            this.y = y;
 
-	getDirections(t)
-	{
-		this.newDirections();
-		var found = [];
+            for (var i in eatArr) {
+                if (x == eatArr[i].x && y == eatArr[i].y) {
+                    eatArr.splice(i, 1);
+                }
 
-		for(var i in this.directions)
-		{
-			var x = this.directions[i][0];
-			var y = this.directions[i][1];
-			if(x >= 0 && x < matrix[0].length && y >=0 && y < matrix.length)
-			{
-				if(matrix[y][x] == t)
-				{
-					found.push(this.directions[i]);
-				}
-			}
-		}
-		return found;
-	}
-
-	move()
-	{
-		console.log(this.energy1);
-		var xotCord = this.getDirections(1);
-		var cord1 = random(xotCord);
-		
-		if(cord1)
-		{
-			var x = cord1[0];
-			var y = cord1[1];
-
-			matrix[y][x] = 4;
-			matrix[this.y][this.x] = 1;
-
-			this.x = x;
-			this.y = y;
-		}
-		else
-		{
-			var emptyCord = this.getDirections(0);	
-			var cord = random(emptyCord);
-			
-				if(cord)
-				{
-					var x = cord[0];
-					var y = cord[1];
-
-					matrix[y][x] = 4;
-					matrix[this.y][this.x] = 0;
-
-					this.x = x;
-					this.y = y;
-				}
-			}
-		}
-	
-
-
-	eat()
-	{		
-	var xotakerCord = this.getDirections(2);
-	var cord1 = random(xotakerCord);
-
-	if(cord1)
-	{
-		
-		var x = cord1[0];
-		var y = cord1[1];
-
-		matrix[this.y][this.x] = 0;	
-		matrix[y][x] = 4;
-
-		this.x = x;
-		this.y = y;
-
-		for(var i in eatArr)
-		{			
-			if(x == eatArr[i].x && y == eatArr[i].y)
-			{
-				eatArr.splice(i,1);
-			}
-
-		} 
-	}
+            }
+        }
 
 
 		/*else if(this.getDirections(0).length != 0)
@@ -126,49 +73,42 @@ class Mard
 			this.move();
 		}*/
 
-		else
-		{
-		this.move();
-		this.energy1--;
-			if(this.energy1 == 0)
-			{
-				var xotakerCord = this.getDirections(1);
-				var cord = random(xotakerCord);
+        else {
+            this.move();
+            this.energy1--;
+            if (this.energy1 == 0) {
+                var xotakerCord = this.getDirections(1);
+                var cord = random(xotakerCord);
 
-				if(cord)	
-				{
-					
-					var x = cord[0];
-					var y = cord[1];
+                if (cord) {
 
-					matrix[this.y][this.x] = 0;	
-					matrix[y][x] = 4;
+                    var x = cord[0];
+                    var y = cord[1];
 
-					this.x = x;
-					this.y = y;
-				}
+                    matrix[this.y][this.x] = 0;
+                    matrix[y][x] = 4;
 
-				this.die();
-			}
-			}
-		}
-	
-	
-    mul()
-    {
-		
-	}
+                    this.x = x;
+                    this.y = y;
+                }
 
-	die()
-	{
-		console.log("merav");
-		matrix[this.y][this.x] = 0;
-			for(var i in mardArr)
-			{			
-				if(this.x == mardArr[i].x && this.y == mardArr[i].y)
-				{
-					mardArr.splice(i,1);
-				}
-			} 	
-	}	
+                this.die();
+            }
+        }
+    }
+
+
+    mul() {
+
+    }
+
+    die() {
+        console.log("merav");
+        matrix[this.y][this.x] = 0;
+        for (var i in mardArr) {
+            if (this.x == mardArr[i].x && this.y == mardArr[i].y) {
+                mardArr.splice(i, 1);
+            }
+        }
+    }
 }
